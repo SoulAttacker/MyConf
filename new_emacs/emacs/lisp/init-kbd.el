f19147f2266cf5bb229a521df25c215346fdedc4
SoulAttacker <49908231+SoulAttacker@users.noreply.github.com>
2023-01-27 21:16:53 +0800

    Delete new_emacs directory

--- new_emacs/emacs/lisp/init-kbd.el
+++
@@ -1,8 +0,0 @@
-;; replace command with meta
-(when *is-mac* 
-    (setq mac-command-modifier 'meta)
-    (setq mac-option-modifier 'none))
-
-;; replace yes-or-no with y-or-n
-(defalias 'yes-or-no-p 'y-or-n-p)
-(provide 'init-kbd)
